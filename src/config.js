fa4b2be2dd6a78c09954e5044fcef3166d48a504
hgfc205 <hektorgermain@gmail.com>
2025-05-20 14:14:42 -0700

    Linux

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ config();// Permite utilizar las variables de entorno declaras en el archivo ".e
 export default{
     host: process.env.HOST || "",
     database: process.env.DATABASE || "",
-    user: process.env.USER || "",
+    user: process.env.DB_USER || "",        //coloco DB_USER porque me detecta el user de linux y no el del .env
     password: process.env.PASSWORD || ""
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import app from "./app.js"; // Importamos la aplicacion desde "app.js
 const main = () =>{
     app.listen(app.get("port"));// Preparamos al servidor en escucha en el puerto "3000" asignado en "app"
     console.log(`Servidor en el puerto ${app.get("port")}`);// Texto que nos indica en consola donde esta corriendo el servidor.
+
+    console.log("Credenciales:", {
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+
 };
 
 main();
